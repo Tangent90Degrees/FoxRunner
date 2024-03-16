f67e1aae755917f14f019a52111abd05cad52c78
Victor <victorliu0530us@icloud.com>
2024-03-17 01:10:43 +0800

    Player jumping

--- Assets/Scripts/Controllers/Player.cs
+++ Assets/Scripts/Controllers/Player.cs
@@ -1,17 +1,26 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
-[RequireComponent(typeof(Rigidbody2D), typeof(PlayerInput))]
+[RequireComponent(typeof(Rigidbody2D), typeof(PlayerInput), typeof(PhysicsEnvironment))]
 public class Player : MonoBehaviour
 {
     /// <summary>
     /// The walking speed of player.
     /// </summary>
     [SerializeField] private float speed;
+
+    [SerializeField] private float jumpForce;
     
     private void Awake()
     {
         _rigidBody = GetComponent<Rigidbody2D>();
         _input = GetComponent<PlayerInput>();
+        _environment = GetComponent<PhysicsEnvironment>();
+    }
+
+    private void OnEnable()
+    {
+        _input.JumpPressed += Jump;
     }
 
     private void FixedUpdate()
@@ -26,7 +35,14 @@ public class Player : MonoBehaviour
             _ => playerTransform.localScale
         };
     }
+    
+    private void Jump(InputAction.CallbackContext _)
+    {
+        if (!_environment.IsOnGround) return;
+        _rigidBody.velocity = new Vector2(_rigidBody.velocity.x, jumpForce);
+    }
 
     private Rigidbody2D _rigidBody;
     private PlayerInput _input;
+    private PhysicsEnvironment _environment;
 }
--- Assets/Scripts/Inputs/PlayerInput.cs
+++ Assets/Scripts/Inputs/PlayerInput.cs
@@ -1,10 +1,18 @@
+using System;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 /// <summary>
 /// Input managing component for players.
 /// </summary>
 public class PlayerInput : MonoBehaviour
 {
+    public event Action<InputAction.CallbackContext> JumpPressed
+    {
+        add => _input.Gameplay.Fire.started += value;
+        remove => _input.Gameplay.Fire.started -= value;
+    }
+    
     /// <summary>
     /// The vector2 value read from player move input.
     /// </summary>
