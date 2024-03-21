73830c84e2adf5034b7b85eaf3750afcf82ecdca
Victor <victorliu0530us@icloud.com>
2024-03-21 19:06:45 +0800

    Add documents

--- Assets/Scripts/Controllers/Player.cs
+++ Assets/Scripts/Controllers/Player.cs
@@ -3,11 +3,14 @@ using System.Collections;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
+/// <summary>
+/// Player component.
+/// </summary>
 [RequireComponent(typeof(Rigidbody2D))]
 public class Player : MonoBehaviour
 {
     #region On Inspector
-    
+
     [SerializeField] private float speed;
     [SerializeField] private float crouchSpeedFactor;
 
@@ -19,14 +22,13 @@ public class Player : MonoBehaviour
     private PhysicsEnvironmentChecker ground;
 
     [SerializeField] private PhysicsEnvironmentChecker head;
-    
+
     #endregion
 
-    public Vector2 Velocity
-    {
-        get => _rigidBody.velocity;
-        private set => _rigidBody.velocity = value;
-    }
+    /// <summary>
+    /// The velocity of this player.
+    /// </summary>
+    private Vector2 Velocity { get => _rigidBody.velocity; set => _rigidBody.velocity = value; }
 
     private void Awake()
     {
@@ -60,9 +62,9 @@ public class Player : MonoBehaviour
         _isCrouching = ground.Hit && (_moveInput.y < 0 || head.Hit);
         (_isCrouching ? _crouchColliderState : _colliderState).Set(_collider);
         head.Origin = _isCrouching ? _crouchColliderState.Top : _colliderState.Top;
-        
-        _animator.SetFloat(AnimatorVariable.Horizontal, Mathf.Abs(_moveInput.x));
-        _animator.SetFloat(AnimatorVariable.Vertical, _rigidBody.velocity.y);
+
+        _animator.SetFloat(AnimatorVariable.Horizontal, _moveInput.x);
+        _animator.SetFloat(AnimatorVariable.Vertical, Velocity.y);
         _animator.SetBool(AnimatorVariable.Ground, ground.Hit);
         _animator.SetBool(AnimatorVariable.Crouch, _isCrouching);
     }
@@ -74,7 +76,7 @@ public class Player : MonoBehaviour
         var position = playerTransform.position;
         ground.HitRay(position, Vector2.down);
         head.HitRay(position, Vector2.up);
-        
+
         var speedFactor = _isCrouching ? crouchSpeedFactor : 1;
         Velocity = new Vector2(speedFactor * speed * _moveInput.x, Velocity.y);
 
@@ -87,7 +89,7 @@ public class Player : MonoBehaviour
 
         KeepJumping();
     }
-    
+
     private void OnDrawGizmos()
     {
         var position = transform.position;
@@ -133,50 +135,97 @@ public class Player : MonoBehaviour
     private ColliderState _colliderState;
     private ColliderState _crouchColliderState;
 
+
     /// <summary>
     /// A physics environment check point.
     /// </summary>
     [Serializable]
     private class PhysicsEnvironmentChecker
     {
-        [field: SerializeField] public Vector2 Origin { get; set; }
-        [field: SerializeField] public float Range { get; private set; }
-        [field: SerializeField] public LayerMask Layer { get; private set; }
-        
+        /// <summary>
+        /// The offset of ray origin.
+        /// </summary>
+        [field: SerializeField]
+        public Vector2 Origin { get; set; }
+
+        /// <summary>
+        /// The maximum distance this checker can detects.
+        /// </summary>
+        [field: SerializeField]
+        public float Range { get; private set; }
+
+        /// <summary>
+        /// The checkable layers.
+        /// </summary>
+        [field: SerializeField]
+        public LayerMask Layer { get; private set; }
+
+        /// <summary>
+        /// The information about an object detected by this checker.
+        /// </summary>
         public RaycastHit2D Hit { get; private set; }
 
+        /// <summary>
+        /// Detects physics environment by 2D ray. This method should be called once pre frame.
+        /// </summary>
+        /// <param name="position">The current position of object.</param>
+        /// <param name="direction">The direction of this ray.</param>
+        /// <returns>The information about an object detected by this checker.</returns>
         public RaycastHit2D HitRay(Vector2 position, Vector2 direction)
         {
             Hit = Physics2D.Raycast(position + Origin, direction, Range, Layer);
             return Hit;
         }
 
+        /// <summary>
+        /// Draws ray gizmo.
+        /// </summary>
+        /// <param name="position">The current position of object.</param>
+        /// <param name="direction">The direction of this ray.</param>
         public void DrawRay(Vector2 position, Vector2 direction)
         {
             Gizmos.DrawLine(position + Origin, position + Origin + Range * direction);
         }
     }
-    
+
+
+    /// <summary>
+    /// The size and offset information of a collider.
+    /// </summary>
     private class ColliderState
     {
+        /// <summary>
+        /// The top point of this collider state.
+        /// </summary>
         public Vector2 Top => _offset + new Vector2(0, 0.5f * _size.y);
-        
+
+        /// <summary>
+        /// Constructs a collider state by specified size. Offset is set that bottom point is origin.
+        /// </summary>
+        /// <param name="size">The size of this collider.</param>
         public ColliderState(Vector2 size)
         {
             _size = size;
             _offset = new Vector2(0, 0.5f * size.y);
         }
 
+        /// <summary>
+        /// Sets a capsule collider to this state.
+        /// </summary>
         public void Set(CapsuleCollider2D collider)
         {
             collider.size = _size;
             collider.offset = _offset;
         }
-        
+
         private readonly Vector2 _size;
         private readonly Vector2 _offset;
     }
+
     
+    /// <summary>
+    /// Variable IDs of the animator.
+    /// </summary>
     private static class AnimatorVariable
     {
         public static readonly int Horizontal = Animator.StringToHash("Horizontal");
